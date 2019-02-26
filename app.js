ef32ed3b17981c87bb9d265109e601e0cc43af82
Chris Cai <caihuijigood@gmail.com>
2019-02-26 13:23:53 +0800

    11
    11

--- app.js
+++ app.js
@@ -1,43 +1,74 @@
-const Koa = require('koa');
-var cron = require('node-cron');
-const app = new Koa();
+const cheerio = require('cheerio');
+const axios = require('axios');
 
 
-var setTimeoutMap = {};
+const getInfo = function (url) {
+    return axios.get(url).then((response) => {
+        const $ = cheerio.load(response.data);
+        var result = {items: [], title: '' , url : url};
+        result.title = $('.archive_title').get(0).firstChild.nodeValue.replace(/\s+?/gi, '');
 
-
-var processGet = function (obj){
-    var msgArr = obj.msg.split('|');
-    if(msgArr.length !=3){
-        return '';
-    }
-    var msgObj = {
-        'replyMember' : msgArr[0],
-        'replyMessage' : msgArr[1],
-        'replyDate' : msgArr[2],
-    }
-    var nowDate = Date.now();
-    var nowYear = nowDate.getFullYear() + '-' +(nowDate.getMonth()+1) +'-'+ nowDate.getDate() ;
-    if(msgObj.replyDate){
-        var goReplyDate ;
-        if(!isNaN(new Date(msg.replyDate) )){
-            goReplyDate = nowDate - new Date(msg.replyDate);
-        } else if (!isNaN(new Date( nowYear +' '+ msg.replyDate ) )){
-            goReplyDate = nowDate - new Date( nowYear +' '+ msg.replyDate );
-        } else if (cron.validate(msg.replyDate)){
-            var task = cron.schedule(msg.replyDate, () =>  {
-                console.log('will not execute anymore, nor be able to restart');
-            });
-        }
-
-
-        if (!goReplyDate || goReplyDate < 0) {
-            throw new Error('Invalid Date');
-        }
-    }
+        $('.grid-item').each((i, elem) => {
+            result.items.push({
+                title: $(elem).find('.title h1').text(),
+                desc: $(elem).find('.title h2').text(),
+                href: $(elem).find('a').attr('href'),
+            })
+        })
+        return result;
+    }).catch((error) => {
+        console.log(error);
+        return {};
+    });
 }
-app.use(async ctx => {
-    ctx.body = 'Hello World';
-});
 
-app.listen(3000);
+
+axios.all([
+    getInfo('https://www.underconsideration.com/brandnew/archives/type/before-after/index.php?page=1'),
+    getInfo('https://www.underconsideration.com/brandnew/archives/type/friday-likes/index.php?page=1'),
+]).then((results) => {
+    var toWebhook = [];
+    results.forEach((item) => {
+
+        if (item.items.length) {
+            toWebhook.push('## '+ item.title);
+            toWebhook.push('\n')
+            var index = 1;
+            item.items.forEach((item) => {
+                // toWebhook.push('[' + item.title + '](' + item.href + ')');
+
+                if (item.title && item.desc) {
+                    toWebhook.push(index + '、'+ item.title + '. [detail](' + item.href + ')');
+                    // toWebhook.push('> ' + item.desc + '.');
+                } else {
+                    toWebhook.push(index + '、'+ item.desc + '. [detail](' + item.href + ')');
+                    // toWebhook.push( '> no description.' );
+                }
+                index++;
+                toWebhook.push('\n')
+            });
+            toWebhook.push('> [more]('+ item.url +')')
+            toWebhook.push('\n')
+        }
+
+    })
+
+    console.log( toWebhook.join('\n'))
+
+
+    // return ;;
+    axios.post('https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=5a3946a2-3234-4b24-9c0f-67d938ee1cdd', {
+        "msgtype": "markdown",
+       // "chatid": "wrkSFfCgAALFgnrSsWU38puiv4yvExuw",
+        "markdown": {
+            "content": toWebhook.join('\n'),
+        }
+    })
+        .then(function (response) {
+            //console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+})
